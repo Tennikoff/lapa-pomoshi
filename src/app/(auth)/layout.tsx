684516e5e50b077965a2169a05c4b585b42af86e
Веретенников Егор <etennikoff@gmail.com>
2026-05-25 14:22:53 +0500

    Deploy

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import type { ReactNode } from "react";
 import styles from "./auth.module.css";
 import { LandingPage } from "../../components/landing/LandingPage";
